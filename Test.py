9d68e34d25ad19df8d7caa8cff19c21cb1af2dd1
TryFailCryTryAgain <david.k.segerbo@gmail.com>
2024-09-16 14:28:29 +0200

    Updated Files

--- Test.py
+++ Test.py
@@ -4,3 +4,5 @@ def Test():
     print("Test")
     
 Test()
+
+
--- side.py
+++ side.py
@@ -1,6 +1,31 @@
 # This is a side python file
+import os
+import random
+import time
 
-def Side():
-    print("This is a side file")
-    
-Side()
+def Clear_Console():
+    x = 1
+    for _ in range(25):
+        os.system('cls||clear')
+        print(x, "%", sep="", end="")
+        x += 1
+        y = random.uniform(0.2, 1)
+        time.sleep(y)
+    for _ in range(50):
+        os.system('cls||clear')
+        print(x, "%", sep="", end="")
+        x += 1
+        y = random.uniform(0.1, 0.2)
+        time.sleep(y)
+    for _ in range(25):
+        os.system('cls||clear')
+        print(x, "%", sep="", end="")
+        x += 1
+        y = random.uniform(0, 0.01)
+        time.sleep(y)        
+    os.system('cls||clear')
+    print("Loading complete! ")
+        
+Clear_Console()
+
+        
