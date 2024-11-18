8f16fb491e808fb75d51f9c2eb6d3f807171d0cd
TryFailCryTryAgain <david.k.segerbo@gmail.com>
2024-11-19 00:29:02 +0100

    updated files

--- PHP_Project/PHP_2/src/User.php
+++ PHP_Project/PHP_2/src/User.php
@@ -3,28 +3,11 @@ interface Authenticate {
     static function authenticate();
 }
 
-class User{
+class User {
     protected string $username;
     protected string $email;
     protected bool $isAdmin;
-    
-    function __construct($username, $email) {
-        $this->username = $username;
-        $this->email = $email;
-        $this->isAdmin = false;
-    }
-
-    public function isAdmin(): bool {
-        return $this->isAdmin;
-    }
-
-    static function authenicate() 
-    {
-        echo "User Authenicated";
-    }
 }
-
-
 
 class Admin extends User {
     private function getUsers() {
